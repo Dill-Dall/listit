348c92156f9988def4c3330a3b473865a2ff00b5
Thomas Dahll <t.a.dahll@gmail.com>
2020-12-08 16:28:55 +0100

    first commit

---
+++ src/main/java/com/demo/Listit/ListItApplication.java
@@ -0,0 +1,13 @@
+package com.demo.Listit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/Listit/ListItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.Listit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
